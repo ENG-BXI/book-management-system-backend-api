9c15e65b24966f5268e9d2c6d2adec5bc7c0ee40
ENG_BXI <eng.0.bxi@gmail.com>
2026-01-14 19:06:37 +0300

    init project and add modules

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { AuthorModule } from './author/author.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [BookModule, AuthorModule, UserModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
